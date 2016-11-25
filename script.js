1dc8741c1a130743802f63173c6f26c7194cafd2
ridler <forrest.ridler@colorado.edu>
2016-11-25 11:32:50 -0700

    remove es6 features and use es5 script

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function loadRoute(route) {
-  var active = route;
-  var anchors = document.getElementsByClassName('nav-link');
-  for (var i = 0; i < anchors.length; i++) {
-    anchors[i].innerHTML = anchors[i].innerHTML.split(' ')[0];
-  }
-
-  event.target.innerHTML += ' <span class="active-anchor">✶</span>';
-
-  for (var ele of document.getElementsByClassName('route-content')) {
-    if (ele.id !== route) {
-      ele.style.display = 'none';
+    var active = route;
+    var anchors = document.getElementsByClassName('nav-link');
+    for (var _i = 0, anchors_1 = anchors; _i < anchors_1.length; _i++) {
+        var el = anchors_1[_i];
+        el.innerHTML = el.innerHTML.split(' ')[0];
     }
-  }
-  document.getElementById(route).style.display = 'block';
-  window.scrollTo(0, 0);
+    event.target.innerHTML += ' <span class="active-anchor">✶</span>';
+    for (var _a = 0, _b = document.getElementsByClassName('route-content'); _a < _b.length; _a++) {
+        var el = _b[_a];
+        if (el.id !== route) {
+            el.style.display = 'none';
+        }
+    }
+    document.getElementById(route).style.display = 'block';
+    window.scrollTo(0, 0);
 }
